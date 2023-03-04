f86ef5fac3cb57f372b3483bd477be3406a3b4f9
ngukhie <khiem-jan.nguyen@mercedes-benz.com>
2023-03-04 16:55:59 +0100

    choire: build app, added index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.get('/api', (req, res) => {
+   res.send('server working');
+});
+
+app.listen(port, () => {
+   console.log(`server started at hhtp://localhost:${port}`); 
+});
