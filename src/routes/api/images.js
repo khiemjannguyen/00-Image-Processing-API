494df811b2043167fdb3bf999b50080172b534c5
ngukhie <khiem-jan.nguyen@mercedes-benz.com>
2023-03-07 16:49:15 +0100

    feat: Add response messages

--- src/routes/api/images.js
+++ src/routes/api/images.js
@@ -6,16 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const createResizedImage_1 = __importDefault(require("../../utilities/createResizedImage"));
 const images = express_1.default.Router();
-images.get("/", (req, res) => {
-    res.send("Image route");
+const requiredParams = ['filename', 'height', 'width'];
+let resizedImagePath;
+images.get('/', (req, res) => {
     try {
+        console.log(typeof (req.query));
         const fileName = req.query.filename;
         const height = parseInt(req.query.height);
         const width = parseInt(req.query.width);
-        (0, createResizedImage_1.default)(fileName, height, width);
+        resizedImagePath = (0, createResizedImage_1.default)(fileName, height, width);
+        images.use('/', express_1.default.static(resizedImagePath));
     }
     catch (error) {
-        console.log(error);
+        if (!req.query.contains) {
+            res.status(400).send('Please make sure url contains correct filename, height and width params');
+            return;
+        }
     }
 });
 exports.default = images;
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,20 +1,52 @@
 import express from "express";
+import {promises as fsPromises} from 'fs';
 import createResizedImage from "../../utilities/createResizedImage";
 
 const images = express.Router();
 
-images.get("/", (req, res) => {
-  res.send("Image route");
+const requiredParams = ['filename', 'height', 'width']
+
+let resizedImagePath: string
+
+images.get('/', (req, res) => {
   try {
+
+    if (!req.query.filename || !req.query.height || !req.query.width) {
+      res.status(400).send('URL contains invalid filename, height or width params!');
+      return;
+    }
+
     const fileName = req.query.filename as string;
     const height = parseInt(req.query.height as unknown as string);
     const width = parseInt(req.query.width as unknown as string);
   
-    createResizedImage(fileName, height, width)
+    resizedImagePath = createResizedImage(fileName, height, width);
+
+    if (resizedImagePath) {
+      fsPromises.readFile(resizedImagePath)
+        .then((thumbData: Buffer) => {
+          res.status(200).contentType('jpg').send(thumbData);
+        })
+        .catch(() => {
+          res.status(500).send('Error occured wihle processing image');
+        });
+
+    } else if (!resizedImagePath) {
+      res.status(404).send('Given image path does not exist');
+      return;
+    }
+
+    images.use('/', express.static(resizedImagePath))
     
   } catch (error) {
-    console.log(error)
+    if (!req.query.filename || !req.query.height || !req.query.width) {
+        res.status(400).send('URL contains invalid filename, height or width params!');
+        return;
+    }
+
   }
 });
+
+
 
 export default images;
