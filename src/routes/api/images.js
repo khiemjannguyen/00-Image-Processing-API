5c1450dbb9bb3e1194b7cce7d5f706d707abd7a5
ngukhie <khiem-jan.nguyen@mercedes-benz.com>
2023-03-07 21:21:25 +0100

    fix: Fix prettier config file

--- src/routes/api/images.js
+++ src/routes/api/images.js
@@ -1,63 +1,106 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const fs_1 = require("fs");
-const createResizedImage_1 = __importDefault(require("../../utilities/createResizedImage"));
-const images = express_1.default.Router();
-const requiredParams = ['filename', 'height', 'width'];
-const getImageFile = (imagePath) => __awaiter(void 0, void 0, void 0, function* () {
-    fs_1.promises.readFile(yield imagePath)
-        .then((thumbData) => {
-        return thumbData;
-    })
-        .catch(() => {
-        console.error;
-    });
-});
-images.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        if (!req.query.filename || !req.query.height || !req.query.width) {
-            res.status(400).send('URL contains invalid filename, height or width params!');
-            return;
+'use strict'
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value)
+                  })
         }
-        const fileName = req.query.filename;
-        const height = parseInt(req.query.height);
-        const width = parseInt(req.query.width);
-        const resizedImagePath = (0, createResizedImage_1.default)(fileName, height, width);
-        console.log('resizedImagePath');
-        console.log(resizedImagePath);
-        if (resizedImagePath) {
-            fs_1.promises.readFile(yield resizedImagePath)
-                .then((thumbData) => {
-                res.status(200).contentType('jpg').send(thumbData);
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value))
+                } catch (e) {
+                    reject(e)
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator['throw'](value))
+                } catch (e) {
+                    reject(e)
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected)
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            )
+        })
+    }
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const fs_1 = require('fs')
+const createResizedImage_1 = __importDefault(
+    require('../../utilities/createResizedImage')
+)
+const images = express_1.default.Router()
+const requiredParams = ['filename', 'height', 'width']
+const getImageFile = (imagePath) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+        fs_1.promises
+            .readFile(yield imagePath)
+            .then((thumbData) => {
+                return thumbData
             })
-                .catch(() => {
-                console.error;
-                res.status(500).send('Error occured wihle processing image');
-            });
+            .catch(() => {
+                console.error
+            })
+    })
+images.get('/', (req, res) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            if (!req.query.filename || !req.query.height || !req.query.width) {
+                res.status(400).send(
+                    'URL contains invalid filename, height or width params!'
+                )
+                return
+            }
+            const fileName = req.query.filename
+            const height = parseInt(req.query.height)
+            const width = parseInt(req.query.width)
+            const resizedImagePath = (0, createResizedImage_1.default)(
+                fileName,
+                height,
+                width
+            )
+            console.log('resizedImagePath')
+            console.log(resizedImagePath)
+            if (resizedImagePath) {
+                fs_1.promises
+                    .readFile(yield resizedImagePath)
+                    .then((thumbData) => {
+                        res.status(200).contentType('jpg').send(thumbData)
+                    })
+                    .catch(() => {
+                        console.error
+                        res.status(500).send(
+                            'Error occured wihle processing image'
+                        )
+                    })
+            } else if (!resizedImagePath) {
+                res.status(404).send('Given image path does not exist')
+                return
+            }
+        } catch (error) {
+            if (!req.query.filename || !req.query.height || !req.query.width) {
+                res.status(400).send(
+                    'URL contains invalid filename, height or width params!'
+                )
+                return
+            }
         }
-        else if (!resizedImagePath) {
-            res.status(404).send('Given image path does not exist');
-            return;
-        }
-    }
-    catch (error) {
-        if (!req.query.filename || !req.query.height || !req.query.width) {
-            res.status(400).send('URL contains invalid filename, height or width params!');
-            return;
-        }
-    }
-}));
-exports.default = images;
+    })
+)
+exports.default = images
--- src/tests/imageSpec.js
+++ src/tests/imageSpec.js
@@ -1,18 +1,24 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const createResizedImage_1 = __importDefault(require("../utilities/createResizedImage"));
+'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+const createResizedImage_1 = __importDefault(
+    require('../utilities/createResizedImage')
+)
 describe('Tests for image manipulation', () => {
-    const imageName = 'test.jpg';
-    const imageFalseName = 'false.jpg';
+    const imageName = 'test.jpg'
+    const imageFalseName = 'false.jpg'
     describe('dunction resizeImage resizes an image with the given width and length', () => {
         it('should create a image copy with the size 200x200', () => {
-            expect((0, createResizedImage_1.default)(imageName, 200, 200)).toBeTruthy;
-        });
+            expect((0, createResizedImage_1.default)(imageName, 200, 200))
+                .toBeTruthy
+        })
         it('should create a image copy with the size 200x200', () => {
-            expect((0, createResizedImage_1.default)(imageFalseName, 200, 200)).toBeFalsy;
-        });
-    });
-});
+            expect((0, createResizedImage_1.default)(imageFalseName, 200, 200))
+                .toBeFalsy
+        })
+    })
+})
